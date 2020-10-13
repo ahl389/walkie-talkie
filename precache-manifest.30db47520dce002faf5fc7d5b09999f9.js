04d04d53bdd5754f265149e0ef0ec368cf7678ea
aboucher <aboucher@twilio.com>
2020-10-13 15:53:41 -0700

    Updates

---
+++ precache-manifest.30db47520dce002faf5fc7d5b09999f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a407f12d1ab9fd86fae30ca9250f1f7b",
+    "url": "/walkie-talkie/index.html"
+  },
+  {
+    "revision": "9d6ce46973511d1d5bc0",
+    "url": "/walkie-talkie/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d9d9e6d40be4159191e8",
+    "url": "/walkie-talkie/static/js/2.9398634f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/walkie-talkie/static/js/2.9398634f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d6ce46973511d1d5bc0",
+    "url": "/walkie-talkie/static/js/main.8de4bcde.chunk.js"
+  },
+  {
+    "revision": "c0c02d96d0cc2f9588d1",
+    "url": "/walkie-talkie/static/js/runtime-main.45ba3799.js"
+  }
+]);
