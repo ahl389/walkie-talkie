6479287b230d2ae2616b20eb87d1a0fb238fc987
aboucher <aboucher@twilio.com>
2020-10-13 15:50:23 -0700

    added password proteection

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ class App extends Component {
 
     this.state={
       identity: '',
-      status: 'offline',
-      ready: false
+      status: '',
+      ready: false,
+      authorized: false,
+      password: ''
     }
 
     this.connect = this.connect.bind(this);
     this.disconnect = this.disconnect.bind(this);
     this.onChangeUpdateState = this.onChangeUpdateState.bind(this);
+    this.typePassword = this.typePassword.bind(this);
+    this.authorize = this.authorize.bind(this);
     this.setup = this.setup.bind(this);
   }
 
@@ -76,7 +80,7 @@ class App extends Component {
 
         this.setState({
           status: 'online',
-          recipient: this.state.identity == 'ashley' ? 'cambell' : 'ashley'
+          recipient: this.state.identity == 'friend1' ? 'friend2' : 'friend1'
         });
       })
       .catch(err => console.log(err));
@@ -88,29 +92,67 @@ class App extends Component {
     })
   }
 
+  typePassword(event) {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  authorize(event) {
+    event.preventDefault();
+    
+    if (this.state.password == process.env.REACT_APP_SECRET) {
+      this.setState({
+        authorized: true,
+        status: 'offline'
+      })
+    } else {
+      this.setState({
+        status: 'unauthorized'
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        { 
-          this.state.ready
-          ? <button 
-              onMouseDown={this.connect} 
-              onMouseUp={this.disconnect}>
-                Press to Talk
-            </button> 
-          : <div>
-              <p>Enter your name to begin.</p>
-              <form onSubmit={this.setup}>
-                <input 
-                  type="text" 
-                  value={this.state.identity}
-                  onChange={this.onChangeUpdateState}
-                  placeholder="What's your name?"></input>
-                <input type="submit" value="Begin Session"></input>
-              </form>
+        {
+          this.state.authorized
+          ? <div>
+            { 
+              this.state.ready
+              ? <button 
+                  onMouseDown={this.connect} 
+                  onMouseUp={this.disconnect}>
+                    Press to Talk
+                </button> 
+              : <div>
+                  <p>Enter your name to begin.</p>
+                  <form onSubmit={this.setup}>
+                    <input 
+                      type="text" 
+                      value={this.state.identity}
+                      onChange={this.onChangeUpdateState}
+                      placeholder="What's your name?"></input>
+                    <input type="submit" value="Begin Session"></input>
+                  </form>
+                </div>
+            }
             </div>
+          : <form onSubmit={this.authorize}>
+              <input 
+                type="password" 
+                value={this.state.password}
+                onChange={this.typePassword}
+                placeholder="Password"></input>
+              <input type="submit" value="Enter Password"></input>
+            </form>
         }
         <p>{ this.state.status }</p>
+
+
+        
+       
       </div>
     );
   }
